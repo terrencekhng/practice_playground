bc9272f1b452a36eec3e26ab39cdedcc8aa9db39
terencekhwu <terencekhwu@tencent.com>
2022-03-31 22:29:53 +0800

    add insertion sort

--- common/sort/insertion_sort.js
+++ common/sort/insertion_sort.js
@@ -1,3 +1,20 @@
-function selection_sort(A) {
-
+/**
+ * Insertion sort
+ * @param {number[]} A
+ */
+function insertion_sort(A) {
+  for (let i = 1; i < A.length; ++i) {
+    let j = i - 1;
+    let temp = A[i];
+    while (j >= 0 && A[j] > temp) {
+      A[j + 1] = A[j];
+      j -= 1;
+    }
+    A[j + 1] = temp;
+    console.log(A);
+  }
 }
+
+let testArray = [5,4,1,2,6];
+insertion_sort(testArray);
+console.log(testArray);
