92477c2ebeb42687df780fd6b251f758e0e12ddb
terencekhwu <terencekhwu@tencent.com>
2022-04-01 19:26:15 +0800

    refactor

--- leetcode/p40.js
+++ leetcode/p40.js
@@ -19,41 +19,41 @@ var combinationSum2 = function(candidates, target) {
     counterList.push([Number(a), b]);
   });
 
-  const backtrack = function(remain, curr, counter) {
-    if (remain <= 0) {
-      if (remain === 0) {
-        // Important, must push a copied array, not the reference to the array
-        results.push(comb.slice());
-      }
-      return;
-    }
-
-    for (let nextCurr = curr; nextCurr < counter.length; ++nextCurr) {
-      let entry = counter[nextCurr];
-      let candidate = entry[0];
-      let freq = entry[1];
-
-      if (freq < 0) {
-        continue;
-      }
-
-      // add new element to the current combination
-      // console.log('result: ', results);
-      comb.push(candidate);
-      counter[nextCurr] = [candidate, freq - 1];
-
-      // continue the exploration with the updated combination
-      backtrack(remain - candidate, nextCurr, counter);
-
-      // backtrack the changes, so that we can try another candidates
-      counter[nextCurr] = [candidate, freq];
-      comb.pop();
-    }
-  }
-
-  backtrack(target, 0, counterList);
+  backtrack(comb, target, 0, counterList, results);
   return results;
 };
+
+function backtrack(comb, remain, curr, counter, results) {
+  if (remain <= 0) {
+    if (remain === 0) {
+      // Important, must push a copied array, not the reference to the array
+      results.push(comb.slice());
+    }
+    return;
+  }
+
+  for (let nextCurr = curr; nextCurr < counter.length; ++nextCurr) {
+    let entry = counter[nextCurr];
+    let candidate = entry[0];
+    let freq = entry[1];
+
+    if (freq < 0) {
+      continue;
+    }
+
+    // add new element to the current combination
+    // console.log('result: ', results);
+    comb.push(candidate);
+    counter[nextCurr] = [candidate, freq - 1];
+
+    // continue the exploration with the updated combination
+    backtrack(comb, remain - candidate, nextCurr, counter, results);
+
+    // backtrack the changes, so that we can try another candidates
+    counter[nextCurr] = [candidate, freq];
+    comb.pop();
+  }
+}
 
 
 let testArray = [10,1,2,7,6,1,5];
