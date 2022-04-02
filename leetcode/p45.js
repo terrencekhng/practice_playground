5451f7e89a0d5c35461db17d2b702e750b398489
terencekhwu <terencekhwu@tencent.com>
2022-04-02 10:35:37 +0800

    problem 45 solution added

--- leetcode/p45.js
+++ leetcode/p45.js
@@ -3,7 +3,18 @@
  * @return {number}
  */
 var jump = function(nums) {
-  
+  let currentReachIndex = 0;
+  let maxReachIndex = 0;
+  let totalJump = 0;
+  for (let i = 0; i < nums.length - 1; ++i) {
+    maxReachIndex = Math.max(maxReachIndex, i + nums[i]);
+    if (i === currentReachIndex) {
+      totalJump += 1;
+      currentReachIndex = maxReachIndex;
+    }
+  }
+
+  return totalJump;
 };
 
 let testArray = [2,3,1,1,4];
