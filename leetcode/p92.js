63505b3ceaedb383482c7a482bbaff1e0c9c3e52
terencekhwu <terencekhwu@tencent.com>
2022-04-05 01:21:50 +0800

    solution to problem 92 added

--- leetcode/p92.js
+++ leetcode/p92.js
@@ -12,5 +12,32 @@
  * @return {ListNode}
  */
 var reverseBetween = function(head, left, right) {
-
+  let leftNode = head;
+  let rightNode = head;
+  let counter = 1;
+  while (counter < left) {
+    leftNode = leftNode.next;
+    counter += 1;
+  }
+  rightNode = leftNode;
+  while (left < right) {
+    rightNode = getRightNode(leftNode, right - left);
+    let temp = leftNode.val;
+    leftNode.val = rightNode.val;
+    rightNode.val = temp;
+    leftNode = leftNode.next;
+    left += 1;
+    right -= 1;
+  }
+  return head;
 };
+
+function getRightNode(leftNode, distance) {
+  let counter = 0;
+  let rightNode = leftNode;
+  while (counter < distance) {
+    rightNode = rightNode.next;
+    counter += 1;
+  }
+  return rightNode;
+}
