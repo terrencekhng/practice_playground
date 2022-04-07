e7e51a68e44eb3722eb908d4651361f53f4b6d3e
terencekhwu <terencekhwu@tencent.com>
2022-04-07 11:42:48 +0800

    problem 75 added

---
+++ leetcode/p75.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+
+};
+
+let nums = [2,0,2,1,1,0];
+sortColors(nums);
+console.log(nums);
