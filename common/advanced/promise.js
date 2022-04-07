3e94cd527865c0eb46bc79f3f7511f4a0a4278b7
terencekhwu <terencekhwu@tencent.com>
2022-04-07 18:11:13 +0800

    promise from scratch

--- common/advanced/promise.js
+++ common/advanced/promise.js
@@ -38,34 +38,91 @@ function MyPromise(fn) {
 
 MyPromise.prototype.then = function(onFulfilled = null, onRejected = null) {
   if (this.state === FULFILLED) {
-    if (typeof onFulfilled === 'function') {
-      onFulfilled(this.value);
-    } else {
-      return this.value;
-    }
+    let promise2 = new MyPromise((resolve, reject) => {
+      try {
+        if (typeof onFulfilled === 'function') {
+          // onFulfilled(this.value);
+          // resolve(this.value);
+          let x = onFulfilled(this.value);
+          resolvePromise(promise2, x, resolve, reject);
+        } else {
+          resolve(this.value);
+        }
+      } catch (error) {
+        reject(error);
+      }
+
+      return promise2;
+    });
   }
 
   if (this.state === REJECTED) {
-    if (typeof onRejected === 'function') {
-      onRejected(this.reason);
-    } else {
-      throw this.reason;
-    }
+    let promise2 = new MyPromise((resolve, reject) => {
+      try {
+        if (typeof onRejected === 'function') {
+          // onRejected(this.reason);
+          // resolve();
+          let x = onRejected(this.reason);
+          resolvePromise(promise2, x, resolve, reject);
+        } else {
+          reject(this.reason);
+        }
+      } catch (error) {
+        reject(error);
+      }
+
+      return promise2;
+    });
   }
 
   if (this.state === PENDING) {
-    this.onFulfilledCallbacks.push(onFulfilled);
-    this.onRejectedCallbacks.push(onRejected);
+    let promise2 = new MyPromise((resolve, reject) => {
+      this.onFulfilledCallbacks.push(() => {
+        try {
+          // onFulfilled(this.value);
+          let x = onFulfilled(this.value);
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      });
+      this.onRejectedCallbacks.push(() => {
+        try {
+          // onRejected(this.reason);
+          let x = onRejected(this.reason);
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+    return promise2;
   }
 
   return this;
 }
 
+function resolvePromise(promise, x, resolve, reject) {
+  if (promise === x) {
+    return reject(new TypeError('The promise and the return value are the same!'));
+  }
+  if (x instanceof MyPromise) {
+    x.then((y) => {
+      resolvePromise(promise, y, resolve, reject);
+    }, reject)
+  }
+  else if (typeof x === 'object' || typeof x === 'function') {
+    if (x === null) {
+      return resolve(x);
+    }
+  }
+}
 
 let promise = new MyPromise((resolve) => {
   setTimeout(() => {
     console.log('hello');
     resolve('resolve');
+    return 1;
   }, 3000);
 });
 promise.then(() => {
