11778d322314c54ffb8067533a5511a0f358122c
terencekhwu <terencekhwu@tencent.com>
2022-04-07 15:06:03 +0800

    solution to problem 75 added

--- leetcode/p75.js
+++ leetcode/p75.js
@@ -3,7 +3,22 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  let mid = left;
 
+  while (mid <= right) {
+    if (nums[mid] === 1) {
+      mid += 1;
+    } else if (nums[mid] === 0) {
+      [nums[left], nums[mid]] = [nums[mid], nums[left]];
+      left += 1;
+      mid += 1;
+    } else if (nums[mid] === 2) {
+      [nums[mid], nums[right]] = [nums[right], nums[mid]];
+      right -= 1;
+    }
+  }
 };
 
 let nums = [2,0,2,1,1,0];
