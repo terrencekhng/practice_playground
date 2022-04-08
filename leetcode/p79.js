12955e528a10e5a90ed907f8f2b3a79564412f73
terencekhwu <terencekhwu@tencent.com>
2022-04-08 17:24:59 +0800

    problem 79 added

---
+++ leetcode/p79.js
@@ -0,0 +1,12 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+
+};
+
+let board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]];
+let result = exist(board, 'ABCCED');
+console.log(result);
