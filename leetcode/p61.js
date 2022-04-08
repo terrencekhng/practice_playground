9713c60bda80a3bec7579135abf0ffc79bcbec17
terencekhwu <terencekhwu@tencent.com>
2022-04-08 17:19:15 +0800

    solution to problem 61 added

--- leetcode/p61.js
+++ leetcode/p61.js
@@ -11,5 +11,36 @@
  * @return {ListNode}
  */
 var rotateRight = function(head, k) {
-
+  if (!head) {
+    return head;
+  }
+  let length = 0;
+  let current = head;
+  let tail = null;
+  while (current) {
+    tail = current;
+    current = current.next;
+    length += 1;
+  }
+  let realK = k % length;
+  head = rotateHelper(head, tail, realK, length);
+  return head;
 };
+
+function rotateHelper(head, tail, k, length) {
+  if (k === 0) {
+    return head;
+  }
+  let current = head;
+  let counter = 0;
+  while (counter < length - k - 1) {
+    current = current.next;
+    counter += 1;
+  }
+  tail.next = head;
+  let next = current.next;
+  current.next = null;
+  head = next;
+  return head;
+}
+
