a777fd45ac155ff57d04e162694a8584bbba759b
terencekhwu <terencekhwu@tencent.com>
2022-04-09 20:45:08 +0800

    solution to problem 86 added

--- leetcode/p86.js
+++ leetcode/p86.js
@@ -11,5 +11,23 @@
  * @return {ListNode}
  */
 var partition = function(head, x) {
-
+  let newLessNode = new ListNode();
+  let lessNodeHead = newLessNode;
+  let newLargeNode = new ListNode();
+  let largeNodeHead = newLargeNode;
+  let current = head;
+  while (current) {
+    if (current.val >= x) {
+      newLargeNode.next = new ListNode();
+      newLargeNode = newLargeNode.next;
+      newLargeNode.val = current.val;
+    } else {
+      newLessNode.next = new ListNode();
+      newLessNode = newLessNode.next;
+      newLessNode.val = current.val;
+    }
+    current = current.next;
+  }
+  newLessNode.next = largeNodeHead;
+  return lessNodeHead;
 };
