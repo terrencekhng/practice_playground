b075488bb7da9a26028031e8fa98930dc2e000c1
terencekhwu <terencekhwu@tencent.com>
2022-04-09 13:00:01 +0800

    problem 347 added

---
+++ leetcode/p347.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+
+};
+
+let nums = [1,1,1,2,2,3];
+let k = 2;
+let result = topKFrequent(nums, k);
+console.log(result);
