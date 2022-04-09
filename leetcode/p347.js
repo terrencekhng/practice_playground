9bf624d8fe9914563bf2896247209abb03e5884f
terencekhwu <terencekhwu@tencent.com>
2022-04-09 13:31:19 +0800

    solution to problem 347 added

--- leetcode/p347.js
+++ leetcode/p347.js
@@ -4,7 +4,24 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-
+  let hash = {};
+  let result = [];
+  for (let i = 0; i < nums.length; ++i) {
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = 1;
+    } else {
+      hash[nums[i]] += 1;
+    }
+  }
+  let array = [];
+  Object.entries(hash).forEach(([key, value]) => {
+    array.push([Number(key), value]);
+  });
+  array.sort((a, b) => b[1] - a[1]);
+  for (let i = 0; i < k; ++i) {
+    result.push(array[i][0]);
+  }
+  return result;
 };
 
 let nums = [1,1,1,2,2,3];
