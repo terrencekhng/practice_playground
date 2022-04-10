1113ff9ada1ebfba656bda939f09bbef5c5d13c6
terencekhwu <terencekhwu@tencent.com>
2022-04-10 23:06:28 +0800

    solution to problem 90 added

--- leetcode/p90.js
+++ leetcode/p90.js
@@ -3,9 +3,33 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-
+  let results = [];
+  nums = nums.sort((a, b) => a - b);
+  powerSubset([], nums, {}, results);
+  return results;
 };
 
-let nums = [1,2,2];
+function powerSubset(prefix, rest, hash, results) {
+  if (rest.length === 0) {
+    if (hash[prefix.join('')]) {
+      return;
+    } else {
+      hash[prefix.join('')] = true;
+      results.push(prefix);
+    }
+    return;
+  }
+  if (hash[prefix.join('')]) {
+    return;
+  } else {
+    hash[prefix.join('')] = true;
+    results.push(prefix);
+  }
+  for (let i = 0; i < rest.length; ++i) {
+    powerSubset([...prefix, rest[i]], rest.slice(i + 1), hash, results);
+  }
+}
+
+let nums = [4,4,4,1,4];
 let results = subsetsWithDup(nums);
 console.log(results);
