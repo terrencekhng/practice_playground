e0f9fd8bcc9b0d582ed2b859d5175968677ab615
terencekhwu <terencekhwu@tencent.com>
2022-04-10 20:55:05 +0800

    problem 682 added

---
+++ leetcode/p682.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} ops
+ * @return {number}
+ */
+var calPoints = function(ops) {
+
+};
+
+
+let ops = ["5","-2","4","C","D","9","+","+"];
+let result = calPoints(ops);
+console.log(result);
