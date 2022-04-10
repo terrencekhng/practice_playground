f0dea30c0a6db365a681c3f0d3bf8c899b7e26fc
terencekhwu <terencekhwu@tencent.com>
2022-04-10 20:50:20 +0800

    solution to problem 36 added

--- leetcode/p36.js
+++ leetcode/p36.js
@@ -3,7 +3,45 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
+  let vertical = {};
+  let horizonal = {};
+  for (let i = 0; i < board.length; ++i) {
+    horizonal = {};
+    vertical = {};
+    for (let j = 0; j < board[0].length; ++j) {
+      if (horizonal[board[i][j]]) {
+        return false;
+      }
+      if (vertical[board[j][i]]) {
+        return false;
+      }
+      if (board[i][j] !== '.') {
+        horizonal[board[i][j]] = true;
+      }
+      if (board[j][i] !== '.') {
+        vertical[board[j][i]] = true;
+      }
+    }
+  }
 
+  for (let i = 0; i < board.length; ++i) {
+    for (let j = 0; j < board.length; ++j) {
+      if (i % 3 === 0 && j % 3 === 0) {
+        let innerHash = {};
+        for (let k = 0; k < 3; ++k) {
+          for (let l = 0; l < 3; ++l) {
+            if (innerHash[board[i + k][j + l]]) {
+              return false;
+            }
+            if (board[i + k][j + l] !== '.') {
+              innerHash[board[i + k][j + l]] = true;
+            }
+          }
+        }
+      }
+    }
+  }
+  return true;
 };
 
 let board = [["5","3",".",".","7",".",".",".","."]
@@ -26,3 +64,4 @@ let board = [["5","3",".",".","7",".",".",".","."]
 //   ,[".",".",".",".","8",".",".","7","9"]];
 let result = isValidSudoku(board);
 console.log(result);
+
