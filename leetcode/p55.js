ac29b8bc31293f399da895e220ddcdd506b264d1
terencekhwu <terencekhwu@tencent.com>
2022-04-12 01:48:49 +0800

    solution to problem 55 added

--- leetcode/p55.js
+++ leetcode/p55.js
@@ -3,9 +3,27 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-
+  if (nums.length === 1) {
+    return true;
+  }
+  let maxIndex = 0;
+  for (let i = 0; i < nums.length; ++i) {
+    if (i === maxIndex && nums[i] === 0) {
+      maxIndex = 0;
+      return false;
+    }
+    if (nums[i] + i > maxIndex) {
+     maxIndex = nums[i] + i;
+     if (maxIndex >= nums.length - 1) {
+       return true;
+     }
+    }
+  }
+  return false;
 };
 
-let nums = [2,3,1,1,4];
+let nums = [0,1];
+// let nums = [3,2,1,0,4];
+// let nums = [2,3,1,1,4];
 let result = canJump(nums);
 console.log(result);
