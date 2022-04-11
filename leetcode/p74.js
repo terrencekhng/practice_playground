73d0a61824e6d534605bfeef3c848ab534d9dd8d
terencekhwu <terencekhwu@tencent.com>
2022-04-11 16:23:22 +0800

    solution to problem 74 added

--- leetcode/p74.js
+++ leetcode/p74.js
@@ -4,10 +4,36 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-
+  if (matrix.length === 1) {
+    return binarySearch(matrix[0], 0, matrix[0].length, target);
+  }
+  let index = null;
+  for (let i = 0, j = 1; j < matrix.length; ++i, ++j) {
+    if (matrix[i][0] <= target && matrix[j][0] > target) {
+      index = i;
+      break;
+    }
+    index = j;
+  }
+  return binarySearch(matrix[index], 0, matrix[index].length, target);
 };
 
-let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]];
-let target = 3;
+function binarySearch(nums, left, right, target) {
+  console.log(left, right);
+  if (left === right) {
+    return false;
+  }
+  let mid = Math.floor((left + right) / 2);
+  if (nums[mid] === target) {
+    return true;
+  } else if (nums[mid] > target) {
+    return binarySearch(nums, left, mid, target);
+  } else if (nums[mid] < target) {
+    return binarySearch(nums, mid + 1, right, target);
+  }
+}
+
+let matrix = [[1,3,5]];
+let target = 4;
 let result = searchMatrix(matrix, target);
 console.log(result);
