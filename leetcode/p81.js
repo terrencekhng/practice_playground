c139880feeb372da9877e08530137e80244ea7c9
terencekhwu <terencekhwu@tencent.com>
2022-04-12 01:02:54 +0800

    problem 81 added

---
+++ leetcode/p81.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+
+};
+
+let nums = [2,5,6,0,0,1,2];
+let target = 0;
+let result = search(nums);
+console.log(result);
