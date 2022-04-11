31bc4c7f0fafeda41b00f5b1586bd2d49e586b08
terencekhwu <terencekhwu@tencent.com>
2022-04-11 12:50:23 +0800

    problem 1260 added

---
+++ leetcode/p1260.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number[][]}
+ */
+var shiftGrid = function(grid, k) {
+
+};
+
+let grid = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]];
+let k = 4;
+let results = shiftGrid(grid, k);
+console.log(results);
