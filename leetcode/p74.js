4bef2f8b5231c44f9e2cb63bc7113d65bedd827d
terencekhwu <terencekhwu@tencent.com>
2022-04-11 15:49:04 +0800

    problem 74 added

---
+++ leetcode/p74.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+
+};
+
+let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]];
+let target = 3;
+let result = searchMatrix(matrix, target);
+console.log(result);
