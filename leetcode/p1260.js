2199a29694fdbd13bd137123fbf7300cf51334ff
terencekhwu <terencekhwu@tencent.com>
2022-04-11 14:42:26 +0800

    solution to problem 1260 added

--- leetcode/p1260.js
+++ leetcode/p1260.js
@@ -4,10 +4,24 @@
  * @return {number[][]}
  */
 var shiftGrid = function(grid, k) {
+  let width = grid[0].length;
+  let height = grid.length;
+  let total = width * height;
+  let realK = k % total;
+  let results = [];
 
+  for (let i = 0; i < height; ++i) {
+    let list = [];
+    results.push(list);
+    for (let j = 0; j < width; ++j) {
+      let index = (i * width + j - realK + total) % total;
+      list.push(grid[Math.floor(index / width)][index % width]);
+    }
+  }
+  return results;
 };
 
-let grid = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]];
-let k = 4;
+let grid = [[1],[2],[3],[4],[7],[6],[5]];
+let k = 23;
 let results = shiftGrid(grid, k);
 console.log(results);
