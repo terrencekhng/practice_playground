f0b87265410d8247258e22300303b0197aff031a
terencekhwu <terencekhwu@tencent.com>
2022-04-12 11:42:32 +0800

    problem 289 added

---
+++ leetcode/p289.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var gameOfLife = function(board) {
+
+};
+
+let board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]];
+gameOfLife(board);
+console.log(board);
