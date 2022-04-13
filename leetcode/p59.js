52fd80ced16089ae5c0951b247c61e2a573d5e36
terencekhwu <terencekhwu@tencent.com>
2022-04-13 12:38:11 +0800

    solution to problem 59 added

--- leetcode/p59.js
+++ leetcode/p59.js
@@ -3,9 +3,45 @@
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
+  if (n === 1) {
+    return [[1]];
+  }
+  let layer = Math.ceil(n / 2);
+  let num = 0;
+  let result = new Array(n);
+  for (let i = 0; i < n; ++i) {
+    result[i] = new Array(n).fill(0);
+  }
 
+  for (let i = 0, count = n - 1; i < layer; ++i, --count) {
+    // deal with the middle one in odd length matrix
+    if (i === count) {
+      result[i][count] = result[i][count - 1] + 1;
+    }
+    // first row
+    for (let j = i; j < count; ++j) {
+      num += 1;
+      result[i][j] = num;
+    }
+    // last col
+    for (let j = i; j < count; ++j) {
+      num += 1;
+      result[j][n - i - 1] = num;
+    }
+    // last row
+    for (let j = n - i - 1; j > i; --j) {
+      num += 1;
+      result[n - i - 1][j] = num;
+    }
+    // first col
+    for (let j = n - i - 1; j > i; --j) {
+      num += 1;
+      result[j][i] = num;
+    }
+  }
+  return result;
 };
 
-let n = 4;
+let n = 9;
 const result = generateMatrix(n);
 console.log(result);
