5bfc94ad06464159cc8389a2866500420ab6d0c2
terencekhwu <terencekhwu@tencent.com>
2022-04-13 14:15:34 +0800

    problem 64 added

---
+++ leetcode/p64.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+
+};
+
+let grid = [[1,3,1],[1,5,1],[4,2,1]];
+let result = minPathSum(grid);
+console.log(result);
