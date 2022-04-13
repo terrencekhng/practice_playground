3125fb9274d8a45cedea1006b5d11743d8676cec
terencekhwu <terencekhwu@tencent.com>
2022-04-13 22:26:24 +0800

    solution to problem 62 added

--- leetcode/p62.js
+++ leetcode/p62.js
@@ -4,7 +4,27 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
+  let dp = [];
+  for (let i = 0; i < m; ++i) {
+    dp[i] = new Array(n).fill(0);
+  }
 
+  for (let i = 0; i < m; ++i) {
+    for (let j = 0; j < n; ++j) {
+      // first row
+      if (i === 0) {
+        dp[i][j] = 1;
+        continue;
+      }
+      // first col
+      if (j === 0) {
+        dp[i][j] = 1;
+        continue;
+      }
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
 };
 
 let m = 3;
