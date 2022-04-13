058b236a53fb0752c517858f68a4908761a7866f
terencekhwu <terencekhwu@tencent.com>
2022-04-13 12:41:26 +0800

    problem 60 added

---
+++ leetcode/p60.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getPermutation = function(n, k) {
+
+};
+
+let n = 4;
+let k = 9;
+let result = getPermutation(n, k);
+console.log(result);
+
