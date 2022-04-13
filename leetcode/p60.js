3fe20eb542fc48d393e934683139ddf65487c732
terencekhwu <terencekhwu@tencent.com>
2022-04-13 14:12:47 +0800

    solution to problem 60 added

--- leetcode/p60.js
+++ leetcode/p60.js
@@ -4,7 +4,32 @@
  * @return {string}
  */
 var getPermutation = function(n, k) {
+  let arr = [];
+  for (let i = 1; i <= n; ++i) {
+    arr.push(i);
+  }
 
+  let result = '';
+  let count = 0;
+
+  function permute(prefix, rest) {
+    if (count === k) {
+      return;
+    }
+    if (rest.length === 0) {
+      count += 1
+      if (count === k) {
+        result = prefix.join('');
+      }
+      return;
+    }
+    for (let i = 0; i < rest.length; ++i) {
+      permute([...prefix, rest[i]], [...rest.slice(0, i), ...rest.slice(i + 1)]);
+    }
+  }
+
+  permute([], arr, count);
+  return result;
 };
 
 let n = 4;
