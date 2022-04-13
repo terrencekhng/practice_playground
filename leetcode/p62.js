bcead2e8b2a2900d1d18038e3e8a2734775d2251
terencekhwu <terencekhwu@tencent.com>
2022-04-13 22:12:08 +0800

    problem 62 added

---
+++ leetcode/p62.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+
+};
+
+let m = 3;
+let n = 7;
+let result = uniquePaths(m, n);
+console.log(result);
