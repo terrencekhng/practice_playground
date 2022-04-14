f7142b701d225527d94a7ab5efc84f585b8098ce
terencekhwu <terencekhwu@tencent.com>
2022-04-14 12:32:46 +0800

    solution to problem 64 added

--- leetcode/p64.js
+++ leetcode/p64.js
@@ -3,9 +3,32 @@
  * @return {number}
  */
 var minPathSum = function(grid) {
+  let width = grid[0].length;
+  let height = grid.length;
 
+  for (let i = 0; i < height; ++i) {
+    for (let j = 0; j < width; ++j) {
+      // first row
+      if (i === 0) {
+        if (j !== 0) {
+          grid[i][j] = grid[i][j] + grid[i][j - 1];
+        }
+        continue;
+      }
+      // first col
+      if (j === 0) {
+        grid[i][j] = grid[i][j] + grid[i - 1][j];
+        continue;
+      }
+      // the rest
+      grid[i][j] = grid[i][j] + Math.min(grid[i - 1][j], grid[i][j - 1]);
+    }
+  }
+  return grid[height - 1][width - 1];
 };
 
-let grid = [[1,3,1],[1,5,1],[4,2,1]];
+// let grid = [[1,3,1],[1,5,1],[4,2,1]];
+// let grid = [[1,2,3],[4,5,6]];
+let grid = [[1,2],[1,1]];
 let result = minPathSum(grid);
 console.log(result);
