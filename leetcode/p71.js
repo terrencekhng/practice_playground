4ad6b7d17451bc8f537915629c7a5b95c6cfcc54
terencekhwu <terencekhwu@tencent.com>
2022-04-16 23:32:31 +0800

    problem 71 added

---
+++ leetcode/p71.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+
+};
+
+let path = '/home//./..';
+let result = simplifyPath(path);
+console.log(result);
