7fa9327b9d72c7a3e43fa0689ee8cc4495a08d7a
terencekhwu <terencekhwu@tencent.com>
2022-04-16 14:09:47 +0800

    solution to problem 538 added

---
+++ leetcode/p538.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST = function(root) {
+  let sum = 0;
+  function traverse(node) {
+    if (node !== null) {
+      traverse(node.right);
+      sum += node.val;
+      node.val = sum;
+      traverse(node.left);
+    }
+    return node;
+  }
+  traverse(root);
+  return root;
+};
+
+
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
+
+let root = new TreeNode(4, new TreeNode(1, new TreeNode(0), new TreeNode(2, null, new TreeNode(3))), new TreeNode(6, new TreeNode(5), new TreeNode(7, null, new TreeNode(8))));
+let node = convertBST(root);
+console.log(node);
+
