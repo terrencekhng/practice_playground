5fa0b0f46bda66fa7b0ae608f01f5553cfa153fe
terencekhwu <terencekhwu@tencent.com>
2022-04-18 13:06:35 +0800

    solution to problem 104 added

--- leetcode/p104.js
+++ leetcode/p104.js
@@ -11,7 +11,20 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-
+  let depth = 0;
+  let max = 0;
+  function traverse(node) {
+    if (!node) {
+      max = Math.max(max, depth);
+      return;
+    }
+    depth += 1;
+    traverse(node.left);
+    traverse(node.right);
+    depth -= 1;
+  }
+  traverse(root);
+  return max;
 };
 
 function TreeNode(val, left, right) {
@@ -21,5 +34,6 @@ function TreeNode(val, left, right) {
 }
 
 let root = new TreeNode(3, new TreeNode(9, new TreeNode(11), new TreeNode(12)), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+// let root = new TreeNode(1, null, new TreeNode(2));
 let depth = maxDepth(root);
 console.log(depth);
