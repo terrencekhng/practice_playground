8661e82ad8941fccd727b688defc8c166fa3cecd
terencekhwu <terencekhwu@tencent.com>
2022-04-18 12:56:19 +0800

    solution to problem 102 added

--- leetcode/p102.js
+++ leetcode/p102.js
@@ -11,7 +11,28 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-
+  let result = [];
+  if (!root) {
+    return [];
+  }
+  let queue = [{node: root, level: 1}];
+  let currentLevel = 0;
+  while (queue.length > 0) {
+    let _node = queue.shift();
+    if (currentLevel === _node.level - 1) {
+      result.push([_node.node.val]);
+    } else {
+      result[result.length - 1].push(_node.node.val);
+    }
+    currentLevel = _node.level;
+    if (_node.node.left) {
+      queue.push({node: _node.node.left, level: _node.level + 1});
+    }
+    if (_node.node.right) {
+      queue.push({node: _node.node.right, level: _node.level + 1});
+    }
+  }
+  return result;
 };
 
 function TreeNode(val, left, right) {
@@ -20,6 +41,6 @@ function TreeNode(val, left, right) {
   this.right = (right===undefined ? null : right)
 }
 
-let root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+let root = new TreeNode(3, new TreeNode(9, new TreeNode(11), new TreeNode(12)), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
 let result = levelOrder(root);
 console.log(result);
