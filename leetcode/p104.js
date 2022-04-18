1b349e551ff346af7a999b394292773ffe7918ef
terencekhwu <terencekhwu@tencent.com>
2022-04-18 12:59:25 +0800

    problem 104 added

---
+++ leetcode/p104.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+
+};
+
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
+
+let root = new TreeNode(3, new TreeNode(9, new TreeNode(11), new TreeNode(12)), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+let depth = maxDepth(root);
+console.log(depth);
