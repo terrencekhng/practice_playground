7094f4554d4165710321e79e0261da0dc269ea1b
terencekhwu <terencekhwu@tencent.com>
2022-04-18 20:40:15 +0800

    problem 105 added

---
+++ leetcode/p105.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+
+};
+
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
+
+let preorder = [];
+let inorder = [];
+let root = buildTree(preorder, inorder);
+console.log(root);
