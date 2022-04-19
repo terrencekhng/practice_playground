bc5b4f33480b281c9941ed86623daa4358f6fcad
terencekhwu <terencekhwu@tencent.com>
2022-04-19 21:19:48 +0800

    solution to problem 113 added

--- leetcode/p113.js
+++ leetcode/p113.js
@@ -12,7 +12,38 @@
  * @return {number[][]}
  */
 var pathSum = function(root, targetSum) {
+  if (!root) {
+    return [];
+  }
+  let result = [];
+  let currentSum = 0;
+  let path = [];
+  function traverse(node) {
+    if (!node) {
+      return;
+    }
 
+    if (currentSum + node.val === targetSum && node && !node.left && !node.right) {
+      path.push(node.val);
+      result.push(path.slice());
+      path.pop();
+      return;
+    }
+
+    path.push(node.val);
+    currentSum += node.val;
+
+    traverse(node.left);
+
+    if (node.right) {
+      traverse(node.right);
+    }
+    path.pop();
+    currentSum -= node.val;
+
+  }
+  traverse(root);
+  return result;
 };
 
 function TreeNode(val, left, right) {
@@ -21,7 +52,8 @@ function TreeNode(val, left, right) {
   this.right = (right===undefined ? null : right)
 }
 
-let root = new TreeNode(5, new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))), new TreeNode(8, new TreeNode(13), new TreeNode(4, new TreeNode(5), new TreeNode(1))));
-let targetSum = 22;
+// let root = new TreeNode(5, new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))), new TreeNode(8, new TreeNode(13), new TreeNode(4, new TreeNode(5), new TreeNode(1))));
+let root = new TreeNode(-2, null, new TreeNode(-3));
+let targetSum = -5;
 let result = pathSum(root, targetSum);
 console.log(result);
