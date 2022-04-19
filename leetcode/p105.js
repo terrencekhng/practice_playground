a6653e4f6117ffb5af0faed78ee7df83367a8f59
terencekhwu <terencekhwu@tencent.com>
2022-04-19 11:08:34 +0800

    solution to problem 105 added

--- leetcode/p105.js
+++ leetcode/p105.js
@@ -12,7 +12,24 @@
  * @return {TreeNode}
  */
 var buildTree = function(preorder, inorder) {
+  let hash = {};
+  for (let i = 0; i < inorder.length; ++i) {
+    hash[inorder[i]] = i;
+  }
 
+  let preorderIndex = 0;
+  function arrayToTree(preorder, left, right) {
+    if (left > right) {
+      return null;
+    }
+    let root = new TreeNode(preorder[preorderIndex++]);
+
+    root.left = arrayToTree(preorder, left, hash[root.val] - 1);
+    root.right = arrayToTree(preorder, hash[root.val] + 1, right);
+
+    return root;
+  }
+  return arrayToTree(preorder, 0, preorder.length - 1);
 };
 
 function TreeNode(val, left, right) {
@@ -21,7 +38,8 @@ function TreeNode(val, left, right) {
   this.right = (right===undefined ? null : right)
 }
 
-let preorder = [];
-let inorder = [];
+let preorder = [1,2,4,5,8,9,3,7];
+let inorder = [4,2,8,5,9,1,7,3];
 let root = buildTree(preorder, inorder);
 console.log(root);
+
