4a27ce715ede829df8f1d099752abb3b51b8978b
terencekhwu <terencekhwu@tencent.com>
2022-05-05 10:35:43 +0800

    Problem 581 added

---
+++ leetcode/p581.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findUnsortedSubarray = function(nums) {
+  
+};
+
+// const nums = [2,6,4,8,10,9,15]; 1 6
+// const nums = [1,3,2,2,2]; 1 4
+// const nums = [1,2,3,3,3];
+const nums = [5,4,3,2,1];
+const result = findUnsortedSubarray(nums);
+console.log(result);
