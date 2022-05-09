af11a1bddcc6f36d09df31b186105ce5cd955e42
terencekhwu <terencekhwu@tencent.com>
2022-05-09 09:44:49 +0800

    Problem 17 added

---
+++ leetcode/p17.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+
+};
+
+const digits = '23';
+const result = letterCombinations(digits);
+console.log(result);
