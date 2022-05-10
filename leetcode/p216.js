f202162fa1f6c5d6e49ce64d1d8f4591316f9913
terencekhwu <terencekhwu@tencent.com>
2022-05-11 02:27:01 +0800

    Problem 216 added

---
+++ leetcode/p216.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function(k, n) {
+
+};
+
+const k = 3;
+const n = 7;
+const result = combinationSum3(k, n);
+console.log(result);
