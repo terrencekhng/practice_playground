0ee8f8a71b0d5e9bd8f1f410f0c661e01c8b2a6c
terencekhwu <terencekhwu@tencent.com>
2022-05-11 14:25:36 +0800

    Solution to problem 216 added

--- leetcode/p216.js
+++ leetcode/p216.js
@@ -4,10 +4,32 @@
  * @return {number[][]}
  */
 var combinationSum3 = function(k, n) {
+  let result = [];
+  const nums = [1,2,3,4,5,6,7,8,9];
+  let hash = {};
 
+  function comb(res, candidate, index) {
+    if (res.length === k) {
+      if (res.reduce((prev, curr) => prev + curr, 0) === n) {
+        const temp = res.sort((a, b) => a - b);
+        if (!hash[temp.join('')]) {
+          hash[temp.join('')] = true;
+          result.push(res);
+        }
+      }
+      return;
+    }
+    for (let i = 0; i < candidate.length; ++i) {
+      comb([...res, candidate[i]], [...candidate.slice(0, i), ...candidate.slice(i + 1)], index + 1);
+    }
+  }
+
+  comb([], nums, 0);
+
+  return result;
 };
 
-const k = 3;
-const n = 7;
+const k = 4;
+const n = 9;
 const result = combinationSum3(k, n);
 console.log(result);
