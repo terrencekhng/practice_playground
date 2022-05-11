03ea9ae1548e1cf961912ac1e6dc381e4a674c81
terencekhwu <terencekhwu@tencent.com>
2022-05-11 14:39:30 +0800

    Another solution(backtracking) to problem 216 added

--- leetcode/p216.js
+++ leetcode/p216.js
@@ -4,32 +4,72 @@
  * @return {number[][]}
  */
 var combinationSum3 = function(k, n) {
-  let result = [];
-  const nums = [1,2,3,4,5,6,7,8,9];
-  let hash = {};
+  // let result = [];
+  // const nums = [1,2,3,4,5,6,7,8,9];
+  // let hash = {};
+  //
+  // function comb(res, candidate, index) {
+  //   if (res.length === k) {
+  //     if (res.reduce((prev, curr) => prev + curr, 0) === n) {
+  //       const temp = res.sort((a, b) => a - b);
+  //       if (!hash[temp.join('')]) {
+  //         hash[temp.join('')] = true;
+  //         result.push(res);
+  //       }
+  //     }
+  //     return;
+  //   }
+  //   for (let i = 0; i < candidate.length; ++i) {
+  //     comb([...res, candidate[i]], [...candidate.slice(0, i), ...candidate.slice(i + 1)], index + 1);
+  //   }
+  // }
+  //
+  // comb([], nums, 0);
+  //
+  // return result;
 
-  function comb(res, candidate, index) {
-    if (res.length === k) {
-      if (res.reduce((prev, curr) => prev + curr, 0) === n) {
-        const temp = res.sort((a, b) => a - b);
-        if (!hash[temp.join('')]) {
-          hash[temp.join('')] = true;
-          result.push(res);
-        }
+  let result = [];
+
+  /**
+   *
+   * @param {number} k
+   * @param {number[]} combination
+   * @param {number} num
+   */
+  function combinationSumHelper(k, combination, num) {
+    if (k === 0) {
+      if (sum(combination) === n) {
+        result.push(combination.slice());
       }
       return;
     }
-    for (let i = 0; i < candidate.length; ++i) {
-      comb([...res, candidate[i]], [...candidate.slice(0, i), ...candidate.slice(i + 1)], index + 1);
+
+    if (num > 9) {
+      return;
     }
+
+    combination.push(num);
+    combinationSumHelper(k - 1, combination, num + 1);
+    combination.pop();
+    combinationSumHelper(k, combination, num + 1);
   }
 
-  comb([], nums, 0);
+  combinationSumHelper(k, [], 1);
 
   return result;
 };
 
-const k = 4;
+/**
+ *
+ * @param {number[]} combination
+ */
+function sum(combination) {
+  return combination.reduce((prev, curr) => prev + curr, 0);
+}
+
+
+
+const k = 3;
 const n = 9;
 const result = combinationSum3(k, n);
 console.log(result);
