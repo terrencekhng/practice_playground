435067509cd59edb2849ff97c8e5d7f16ac7fc33
terencekhwu <terencekhwu@tencent.com>
2022-05-11 15:25:31 +0800

    Solution to problem 1641 added

--- leetcode/p1641.js
+++ leetcode/p1641.js
@@ -3,9 +3,28 @@
  * @return {number}
  */
 var countVowelStrings = function(n) {
+  let count = 0;
 
+  function comb(n, combination, next) {
+    if (n === 0) {
+      count += 1;
+      return;
+    }
+
+    if (next > 4) {
+      return;
+    }
+
+    combination.push(next);
+    comb(n - 1, combination, next);
+    combination.pop();
+    comb(n, combination, next + 1);
+  }
+
+  comb(n, [], 0);
+  return count;
 };
 
-const n = 5;
+const n = 4;
 const result = countVowelStrings(n);
 console.log(result);
